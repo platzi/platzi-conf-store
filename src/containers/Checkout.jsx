5db8b4af776ccd103c21ba1815a07f7c0a85c339
Oscar Barajas Tavares <oscar@arepa.dev>
2020-10-22 16:16:45 -0500

    add checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../styles/components/Checkout.css';
 
 const Checkout = () => {
   return (
-    <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>ITEM name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
   );
 }
 
