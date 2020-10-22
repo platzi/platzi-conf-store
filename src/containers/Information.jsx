346f77f1655542be80f486895a7399a09a0d097b
Oscar Barajas Tavares <oscar@arepa.dev>
2020-10-22 16:29:49 -0500

    add Information

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
   return (
-    <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="text" placeholder="Correo Electronico" name="email" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo postal" name="cp" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">
+            Regresar
+          </div>
+          <div className="Information-next">
+            pagar
+          </div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM Name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
