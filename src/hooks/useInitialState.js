d1b4781a04347f2a5a49cc309622a5c466112011
Oscar Barajas Tavares <oscar@arepa.dev>
2020-10-22 17:53:27 -0500

    add context

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import initialState from '../initialState';
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  const addToCart = payload => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  }
+
+  const removeFromCart = payload => {
+    setState({
+      ...state,
+      cart: state.cart.filter(items => items.id !== payload.id),
+    });
+  };
+
+  return {
+    addToCart,
+    removeFromCart,
+    state,
+  };
+};
+
+export default useInitialState;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,21 +8,26 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
